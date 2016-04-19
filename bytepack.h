03bba068d36ce93bef4998cc1d61fd6139af62cb
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2016-04-19 08:02:45 +0000

    Noted latest change copyright date

--- bytepack.h
+++ bytepack.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
- * Copyright (C) Alexander Kozhevnikov <mentalisttraceur@gmail.com> 2016-04-18
+ * Copyright (C) Alexander Kozhevnikov <mentalisttraceur@gmail.com> 2016-04-19
  * 
  * "bytepack"/"byteback" name idea contributed by Mike Serritella
  *     <author@mikeserritella.com> 2015-09-15
